8c3c2a2a2b88956a100ef8c12e6b4dfc901ab13f
Carl <ldscode@163.com>
2017-02-23 11:06:40 +0800

    树节点的维护,主从树,递归树示例

---
+++ src/main/webapp/tree/ms-tree.js
@@ -0,0 +1,79 @@
+/**
+ * Created by carl.li on 2017/2/16.
+ */
+(function () {
+    cola(function (model) {
+        model.dataType({
+            name: "Product",
+            properties: {
+                productName: {
+                    validators: ["required"]
+                },
+                id: {
+                    dataType: "number"
+                },
+                categoryId: {
+                    dataType: "number"
+                },
+                quantityPerUnit: {
+                    dataType: "number"
+                },
+                unitPrice: {
+                    dataType: "number"
+                },
+                unitsInStock: {
+                    dataType: "number"
+                },
+                unitsOnOrder: {
+                    dataType: "number"
+                }
+            }
+        });
+        model.dataType({
+            name: "Category",
+            properties: {
+                categoryName: {
+                    validators: ["required"]
+                },
+                products: {
+                    dataType: "Product",
+                    provider: {
+                        url: "/service/product/?categoryId={{@id}}",
+                        pageSize: 5
+                    }
+                }
+            }
+        });
+        model.describe("categories", {
+            dataType: "Category",
+            provider: {
+                url: "/service/category/",
+                pageSize: 5
+            }
+        });
+
+        model.action({
+
+        });
+
+        model.widgetConfig({
+            tree: {
+                $type: "tree",
+                autoCollapse: false,
+                autoExpand: true,
+                highlightCurrentItem: true,
+                bind: {
+                    expandedProperty: "expanded",
+                    textProperty: "categoryName",
+                    expression: "category in categories",
+                    child: {
+                        expandedProperty: "expanded",
+                        textProperty: "productName",
+                        expression: "product in category.products"
+                    }
+                }
+            }
+        });
+
+    });
+}).call(this);
---
+++ src/main/webapp/tree/recursive-tree.js
@@ -0,0 +1,71 @@
+/**
+ * Created by carl.li on 2017/2/16.
+ */
+(function() {
+cola(function (model) {
+    model.dataType({
+        name: "Area",
+        properties: {
+            areas: {
+                dataType: "Area",
+                provider: {
+                    url: "./service/area/recursion/?parentId={{@id}}"
+                }
+            }
+        }
+    });
+    model.describe("areas",{
+        dataType: "Area",
+        provider: {
+            url: "./service/area/recursion/?parentId=-1"
+        }
+    });
+
+    model.action({
+        selectedNodes: function (nodes, arrayNode) {
+            $.each(nodes, function () {
+                var node = this;
+                if (node.get("checked")) {
+                    arrayNode.push(node.get("data.areaName"));
+                }
+                var children = node.get("children");
+                if (children) {
+                    model.action.selectedNodes(children, arrayNode);
+                }
+            });
+            model.set("arrayNode", arrayNode);
+        },
+        getNodes: function () {
+            var nodes = cola.widget("recursiveTree").getItems();
+            var arrayNode = [];
+            model.action.selectedNodes(nodes,arrayNode);
+            model.get("arrayNode");
+            if (arrayNode.length > 0) {
+                cola.alert("选中的节点有："+model.get("arrayNode"));
+            } else {
+                cola.alert("请选择树节点！");
+            }
+        }
+    });
+
+    model.widgetConfig({
+        recursiveTree: {
+            $type: "tree",
+            bind: {
+                checkedProperty: "checked",
+                textProperty: "areaName",
+                valueProperty: "id",
+                expression: "area in areas",
+                child: {
+                    recursive: true,
+                    checkedProperty: "checked",
+                    textProperty: "areaName",
+                    valueProperty: "id",
+                    expression: "area in area.areas"
+                }
+            }
+        }
+    });
+
+});
+}).call(this);
