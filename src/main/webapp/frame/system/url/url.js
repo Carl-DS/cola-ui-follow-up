c4ed496661ac3b264481deb3d6249eacedc70f9d
Carl <ldscode@163.com>
2017-03-09 13:59:48 +0800

    修复菜单树节点维护无法给子节点添加节点的bug

--- src/main/webapp/frame/system/url/url.js
+++ src/main/webapp/frame/system/url/url.js
@@ -42,99 +42,101 @@
         });
         model.describe("currentEditItem", "Url");
         model.set("currentEditItem", {});
-        model.set("currentEditItem.forNavigation", "true");
         model.action({
             add: function () {
-                var currentEditItem=model.get("currentEditItem");
-                var nodes = currentEditItem.get("menus");
-                if (!nodes) {
-                    currentEditItem.set("menus", []);
-                    nodes = currentEditItem.get("menus")
+                debugger;
+                var tree, currentEditItem, currentNode, nodeEntity, nodes, order;
+                tree = cola.widget("urlTree");
+                currentEditItem=model.get("currentEditItem");
+                currentNode = tree.findNode(currentEditItem);
+                nodeEntity = currentNode.get("data");
+                if (nodeEntity.state === "new") {
+                    cola.NotifyTipManager.error({
+                        message : "添加失败",
+                        description : "您添加的节点未保存，请先保存再添加子节点！",
+                        showDuration : 3000
+                    });
+                } else {
+                    nodes = currentEditItem.get("menus");
+                    order = 1;
+                    if (!nodes) {
+                        // 将currentEditItem设置成EntityList, 便于使用insert() 方法
+                        currentEditItem.set("menus", []);
+                        nodes = currentEditItem.get("menus")
+                    } else {
+                        if (nodes.entityCount>0)
+                            order = nodes.last().get("order") + 1;
+                    }
+
+                    var entity=nodes.insert({
+                        label : "<新菜单>",
+                        parentId : nodeEntity.get("id"),
+                        order : order,
+                        icon : "icon edit",
+                        forNavigation : true,
+                        companyId : ""
+                    });
+
+                    tree.expand(currentNode);
+                    tree.set("currentItem",entity);
+                    event && event.stopPropagation();
                 }
-                var tree = cola.widget("urlTree");
-                var currentNode = tree.findNode(currentEditItem);
-                tree.expand(currentNode);
-                var entity=nodes.insert({
-                    label : "<新菜单>",
-                    parentId : tree.get("currentNode").get("data").get("id"),
-                    order : 1,
-                    icon : "iconfont icon-shoudan",
-                    forNavigation : true,
-                    companyId : ""
-                });
-                tree.set("currentItem",entity);
-                event && event.stopPropagation();
             },
-            // add : function() {
-            //     debugger;
-            //     var tree = cola.widget("urlTree");
-            //     var currentNode = tree.get("currentNode");
-            //     var nodeEntity = currentNode.get("data");
-            //     if (nodeEntity.state == "new") {
-            //         cola.NotifyTipManager.error({
-            //             message : "添加失败",
-            //             description : "您添加的节点未保存，请先保存再添加子节点！",
-            //             showDuration : 1000
-            //         });
-            //     } else {
-            //         var order;
-            //         var nodes = nodeEntity.get("menus", "sync");
-            //         if (!nodes) {
-            //             nodeEntity.set("menus", []);
-            //             nodes = nodeEntity.get("menus");
-            //         }
-            //         if (!nodeEntity._data.menus._last._last) {
-            //             order = 1;
-            //         } else {
-            //             order = nodeEntity._data.nodes._last._last._data.order + 1;
-            //         }
-            //         var entity = nodes.insert({
-            //             name : "<新菜单>",
-            //             parentId : nodeEntity.get("id"),
-            //             order : order,
-            //             icon : "iconfont icon-shoudan",
-            //             forNavigation : true,
-            //             companyId : ""
-            //
-            //         });
-            //         tree.expand(currentNode);
-            //         tree.set("currentItem", entity);
-            //         event && event.stopPropagation();
-            //     }
-            // },
-            remove : function(node) {
+            remove : function(url) {
                 var currentEditItem = model.get("currentEditItem");
-                var nodes = currentEditItem.get("nodes", "sync");
+                var nodes = currentEditItem.get("menus", "sync");
                 cola.confirm("您确定要删除当前记录吗？", {
                     onApprove : function() {
                         if (nodes && nodes.entityCount > 0) {
-                            cola.alert("存在子菜单，不可删除！");
+                            cola.alert("存在子菜单，无法删除！");
                         } else {
-                            $.ajax("./service/frame/url/"
-                                + model.get("currentEditItem").get(
-                                    "id") + "/", {
-                                type : "DELETE",
-                                success : function(arg, self) {
-                                    node.remove();
-                                    cola.NotifyTipManager.success({
-                                        message : "删除成功",
-                                        description : "后台任务执行成功！",
-                                        showDuration : 1000
-                                    });
-                                }
+                            cola.NotifyTipManager.success({
+                                message : "删除成功",
+                                description : "后台任务执行成功！",
+                                showDuration : 1000
                             });
+                            url.remove();
+                            // $.ajax("./service/frame/url/"
+                            //     + model.get("currentEditItem").get(
+                            //         "id") + "/", {
+                            //     type : "DELETE",
+                            //     success : function(arg, self) {
+                            //         node.remove();
+                            //         cola.NotifyTipManager.success({
+                            //             message : "删除成功",
+                            //             description : "后台任务执行成功！",
+                            //             showDuration : 1000
+                            //         });
+                            //     }
+                            // });
                         }
                     }
                 });
             },
             saveUrl: function () {
                 debugger;
+            },
+            copyNodeDataToEdit: function (currentData) {
+                var forNavigationValue, tempValue;
+                model.set("currentEditItem", currentData);
+                tempValue = currentData.get("forNavigation");
+                if (tempValue) {
+                    if (typeof tempValue === "string")
+                        forNavigationValue = tempValue;
+                    else
+                        forNavigationValue="是";
+                } else {
+                    forNavigationValue="否";
+                }
+                cola.widget("forNavigationRadioGroup").set("value", forNavigationValue);
+                model.set("currentEditItem.forNavigation", forNavigationValue);
             }
+
         });
 
         model.widgetConfig({
             forNavigationRadioGroup: {
-                items: ["true", "false"]
+                items: ["是", "否"]
             },
             urlTree: {
                 $type: "tree",
@@ -154,11 +156,10 @@
                     }
                 },
                 currentNodeChange: function (self, arg) {
-                    var current = self.get("currentNode");
-                    if (current) {
-                        model.set("currentEditItem", current ? current.get("data") : null);
-                        // var entity = current.get("data");
-                        //copyNodeDataToEdit(entity);
+                    var current;
+                    current = self.get("currentNode");
+                    if (cola.defaultAction.isNotEmpty(current)) {
+                        model.action.copyNodeDataToEdit(current.get("data"));
                     }
                 }
             }
