3123c37a4677b4ac98bec7afb6508dbf73b874c3
Carl <ldscode@163.com>
2017-10-09 22:58:08 +0800

    更新spring　framework 版本到4.3

--- src/main/java/com/colaui/example/controller/ColaAreaController.java
+++ src/main/java/com/colaui/example/controller/ColaAreaController.java
@@ -4,10 +4,7 @@ import com.colaui.example.model.ColaArea;
 import com.colaui.example.service.ColaAreaService;
 import com.colaui.helper.Page;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,17 +12,17 @@ import java.util.List;
  * Created by carl.li on 2017/2/14.
  */
 @RestController
-@RequestMapping("area")
+@RequestMapping("/area")
 public class ColaAreaController {
     @Autowired
     private ColaAreaService colaAreaService;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping(value = "")
     public Page<ColaArea> getAreas(@RequestParam int pageSize, @RequestParam int pageNo) {
         return colaAreaService.getAreas(pageSize, pageNo);
     }
 
-    @RequestMapping(value = "/recursion/", method = RequestMethod.GET)
+    @GetMapping(value = "/recursion")
     public List<ColaArea> recursionTree(@RequestParam String parentId) {
         return  colaAreaService.recursionTree(parentId);
     }
--- src/main/java/com/colaui/example/controller/ColaCategoryController.java
+++ src/main/java/com/colaui/example/controller/ColaCategoryController.java
@@ -9,43 +9,43 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("category")
+@RequestMapping("/category")
 public class ColaCategoryController {
-	@Autowired
-	ColaCategoryService categoryService;
+    @Autowired
+    ColaCategoryService categoryService;
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public Page<ColaCategory> paging(@RequestParam int pageSize,
-									 @RequestParam int pageNo,
-									 @RequestParam(required=false) String contain) {
-		return categoryService.getPage(pageSize, pageNo, contain);
-	}
+    @GetMapping(value = "")
+    public Page<ColaCategory> paging(@RequestParam int pageSize,
+                                     @RequestParam int pageNo,
+                                     @RequestParam(required = false) String contain) {
+        return categoryService.getPage(pageSize, pageNo, contain);
+    }
 
-	@RequestMapping(value = "/", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
-	public void save(@RequestBody ColaCategory category) {
-		System.out.println(category.getCategoryName());
-		categoryService.save(category);
-	}
+    @PostMapping(value = "")
+    public void save(@RequestBody ColaCategory category) {
+        System.out.println(category.getCategoryName());
+        categoryService.save(category);
+    }
 
-	@RequestMapping(value = "/{id}/", method = RequestMethod.DELETE)
-	public void delete(@PathVariable("id") long id) {
-		System.out.println("Fetching & Deleting ColaEmployee with id " + id);
-		 categoryService.delete(id);
-	}
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable("id") long id) {
+        System.out.println("Fetching & Deleting ColaEmployee with id " + id);
+        categoryService.delete(id);
+    }
 
-	@RequestMapping(value = "/", method = RequestMethod.PUT, produces = "application/json; charset=utf-8")
-	public void update(@RequestBody ColaCategory category) {
-		System.out.println(category.getCategoryName());
-		categoryService.update(category);
-	}
+    @PutMapping(value = "/")
+    public void update(@RequestBody ColaCategory category) {
+        System.out.println(category.getCategoryName());
+        categoryService.update(category);
+    }
 
-	@RequestMapping(value = "/{id}/", method = RequestMethod.GET)
-	public ColaCategory find(@PathVariable("id") long id) {
-		return categoryService.find(id);
-	}
+    @GetMapping(value = "/{id}/")
+    public ColaCategory find(@PathVariable("id") long id) {
+        return categoryService.find(id);
+    }
 
-	@RequestMapping(value = "/{from}/{limit}", method = RequestMethod.GET)
-	public List<ColaCategory> find(@PathVariable("from") int from, @PathVariable("limit") int limit) {
-		return categoryService.find(from, limit);
-	}
+    @GetMapping(value = "/{from}/{limit}")
+    public List<ColaCategory> find(@PathVariable("from") int from, @PathVariable("limit") int limit) {
+        return categoryService.find(from, limit);
+    }
 }
